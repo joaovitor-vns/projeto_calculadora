9a89e439b6f093596cdd549995079150e342f63f
João Vitor Vale <joaovitornascimento03.jvn@gmail.com>
2024-11-20 19:43:37 -0300

    projeto finalizado calculadora

--- src/script.js
+++ src/script.js
@@ -1,7 +1,34 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+//Função principal
+
+function Calculadora(num1, num2, operador) {
+  let resultado
+
+  num1 = Number(num1)
+  num2 = Number(num2)
+
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "e":
+      resultado = num1 ** num2
+      break;
+  }
+  if (resultado == undefined || resultado > 1000000) {
+    resultado = "ERRO"
+  }
+  return resultado
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
